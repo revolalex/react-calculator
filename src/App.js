5521e6d7bb307ce3cc01caeba859b6fa1f9a7778
dmytro.vakuliuk <dmytro.vakuliuk@backendlessmail.com>
2020-10-06 19:09:34 +0300

    Refactoring using hooks

--- src/App.js
+++ src/App.js
@@ -1,95 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Clock from "./components/Clock";
 
-// import css
 import './App.css';
 
-// child components
 import ResultComponent from './components/ResultComponent';
 import KeyPadComponent from "./components/KeyPadComponent";
 
 
-class App extends Component {
-    constructor(){
-        // call the constructor
-        super();
+function App() {
+    const [result, setResult] = useState('')
 
-        this.state = {
-            result: ""
-        }
-    }
-
-    // Function call on every click, button is the parameter 
-    // with the value of the press button
-    onClick = button => {
-        // Controle the touch press
-        if(button === "="){
-            // call function calculate
-            this.calculate()
-        }
-
-        else if(button === "C"){
-            this.reset()
-        }
-        else if(button === "CE"){
-            this.backspace()
-        }
-
-        else {
-            // setState is use to modify the state (result)
-            this.setState({
-                result: this.state.result + button
-            })
+    const onClick = button => {
+        if (button === "=") {
+            calculate()
+        } else if (button === "C") {
+            reset()
+        } else if (button === "CE") {
+            backspace()
+        } else {
+            setResult(result + button)
         }
     };
 
-
-    // Calculate the result of our expression, 
-    // this is triggered, when “=” button is pressed.
-    calculate = () => {
+    const calculate = () => {
         try {
-            this.setState({
-                // eval(str) allow us to evaluate a character link
-                // eslint-disable-next-line 
-                result: (eval(this.state.result) || "" ) + ""
-            })
+            //eslint-disable-next-line
+            setResult (eval(result || "") + "")
         } catch (e) {
-            this.setState({
-                result: "error"
-            })
-
+            setResult("error")
         }
     };
-    // Clear our output, this is trigered when “C” is pressed.
-    reset = () => {
-        this.setState({
-            result: ""
-        })
-    };
-    // Clear the last character that was pressed. triggered on “CE”.
-    backspace = () => {
-        this.setState({
-            result: this.state.result.slice(0, -1)
-        })
-    };
 
+    const reset = () => setResult("")
+    const backspace = () => setResult(result.slice(0, -1))
 
+    return (
+      <div className="calculator-body">
+          <h1>Calculator</h1>
 
-    // What we see
-    render() {
-        return (
-            <div>
-                <div className="calculator-body">
-                    <h1>Calculator</h1>
-                    <Clock/>
-                    {/* component with props */}
-                    <ResultComponent result={this.state.result}/>
-                    {/* component with function onClick */}
-                    <KeyPadComponent onClick={this.onClick}/>
-                </div>
-            </div>
-        );
-    }
+          <Clock/>
+          <ResultComponent result={ result }/>
+          <KeyPadComponent onClick={ onClick }/>
+      </div>
+    );
 }
 
 export default App;
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,32 +1,20 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Clock extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        time: new Date().toLocaleString()
-      };
-    }
-    componentDidMount() {
-      this.intervalID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-    componentWillUnmount() {
-      clearInterval(this.intervalID);
-    }
-    tick() {
-      this.setState({
-        time: new Date().toLocaleString()
-      });
-    }
-    render() {
-      return (
-        <p className="App-clock">
-           {this.state.time}
-        </p>
-      );
-    }
-  }
-  export default Clock;
+export default function Clock() {
+
+  const [time, setTime] = useState(new Date().toLocaleString())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleString())
+    })
+
+    return function cleanup() {
+      clearInterval(timer)
+    };
+  }, [])
+
+  return (
+    <p className="App-clock">{ time }</p>
+  );
+}
